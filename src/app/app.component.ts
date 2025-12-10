baaab0ea838a99b42991e02442a8d58b10792423
Antonio Barroso <abarrod822@iescarrillo.es>
2025-12-10 14:51:24 +0100

    A

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Componente1Component } from './components/componente1/componente1.component';
 import { Componente2Component } from './components/componente2/componente2.component';
+import { HomeComponent } from './components/home/home.component';
+import { PresentacionComponent } from "./components/presentacion/presentacion.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Componente1Component, Componente2Component],
+  imports: [RouterOutlet, Componente1Component, Componente2Component, HomeComponent, PresentacionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
