1e8a9fa62f5ae0f644760f77f179d7f962af2a16
Antonio Barroso <abarrod822@iescarrillo.es>
2025-12-10 14:31:00 +0100

    A

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Componente1Component } from './componente1/componente1.component';
+import { Componente1Component } from './components/componente1/componente1.component';
+import { Componente2Component } from './components/componente2/componente2.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Componente1Component],
+  imports: [RouterOutlet, Componente1Component, Componente2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,6 @@ import { provideRouter } from '@angular/router';
 
 
 import { routes } from './app.routes';
-import { Componente1Component } from './componente1/componente1.component';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
